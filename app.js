b4a96fa6581f74927b1b90db2f43c46c21c9e588
seriessoft <khaleelbasuny@yahoo.com>
2017-07-03 19:50:10 +0300

    Update app.js

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ var config = {
 };
 /*
 our firebase credential
-Hello there
+Hello there, i am nice
 var config = {
 	apiKey: "AIzaSyBILjsq_QSbE8X-H2tiSxJxX5i1AV139nI",
     authDomain: "monopoly-2ffc6.firebaseapp.com",
