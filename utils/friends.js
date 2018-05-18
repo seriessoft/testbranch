aca386583831f88190cab30144f1c9daad20b465
seriessoft <khaleelbasuny@yahoo.com>
2018-05-18 20:37:33 +0300

    Update friends.js

--- utils/friends.js
+++ utils/friends.js
@@ -1,1 +1,27 @@
+var Firebase = require('firebase');
+var moment = require('moment');
 
+
+var addPlayerToDatabase = function(uid, newFriend){
+  
+  var playerRef = Firebase.database().ref().child('users').child(uid);
+  playerRef.child('friends').child(newFriend).set(newFriend);
+  
+}
+
+var removePlayerFromDatabase = function(uid, friend){
+  
+  var playerRef = Firebase.database().ref().child('users').child(uid);
+  playerRef.child('friends').child(friend).remove();
+  
+}
+
+var findFrineds = function(uid, callback){
+  
+  var playerRef = Firebase.database().ref().child('users').child(uid);
+  playerRef.child('users').once('value').then( function(snap){
+    var friendslist = snap;
+    callback = friendslist;
+  });
+  
+}
