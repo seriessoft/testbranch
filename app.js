1d2f25d523b45de26a8185f8fb1b7363e1d6d2eb
seriessoft <khaleelbasuny@yahoo.com>
2018-05-19 12:32:11 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1042,6 +1042,7 @@ wsServer.on('request', function(request) {
 						break;
 					
 				case "FRIENDS":
+					console.log(reqM.actiontype + " " + reqM.uid + " " +reqM.newFriend);
 					if(reqM.actiontype == "ADD" && reqM.uid && reqM.newfriend){
 						console.log('1'+reqM);
 						Friends.addPlayerToDatabase(reqM.uid, reqM.newFriend);
