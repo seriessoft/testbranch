2dde6cc3f5f336c7161771f69d2bee2e0d7bb54b
seriessoft <khaleelbasuny@yahoo.com>
2018-05-19 12:20:10 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1043,7 +1043,9 @@ wsServer.on('request', function(request) {
 					
 				case "FRIENDS":
 					if(reqM.actiontype == "ADD" && reqM.uid && reqM.newfriend){
+						console.log('1'+reqM);
 						Friends.addPlayerToDatabase(reqM.uid, reqM.newFriend);
+						console.log('1'+reqM);
 						connection.sendUTF(JSON.stringify({calltoken:calltoken,errorcode:0,msg:'successfully added'}));
 					}
 					else if(reqM.actiontype == "REMOVE" && reqM.uid && reqM.friend){
